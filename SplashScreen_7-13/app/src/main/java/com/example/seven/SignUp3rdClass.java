a66dda032cb108cccd20f3df374a007958997c48
Abuessa265 <abuessastudent1999@gmail.com>
2024-01-04 10:51:15 +0600

    third commit

---
+++ SplashScreen_7-13/app/src/main/java/com/example/seven/SignUp3rdClass.java
@@ -0,0 +1,14 @@
+package com.example.seven;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SignUp3rdClass extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_up3rd_class);
+    }
+}
---
+++ SplashScreen_7-13/app/src/main/java/com/example/seven/verifyOTP.java
@@ -0,0 +1,14 @@
+package com.example.seven;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class verifyOTP extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_verify_otp);
+    }
+}
